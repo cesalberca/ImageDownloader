7bc5ae4c39396636ac7045ee3bfc228d161201cb
cesalberca <cesalberca@gmail.com>
2016-11-18 11:24:03 +0100

    Clean up and made it self-contained

--- index.js
+++ index.js
@@ -1,8 +1,87 @@
-const imgDownloader = require('./img-downloader')
-const path = require('path')
-const url = require('url')
+/**
+ * Created by Cesar on 15/11/2016.
+ */
+module.exports = (() => {
+  'Use strict'
 
-let filePath = path.join(__dirname, 'images.txt')
-let downloadFolder = path.join(__dirname, 'imgDownloads')
+  const readline = require('readline')
+  const url = require('url')
+  const path = require('path')
+  const http = require('http')
+  const Promise = require('bluebird')
+  const fs = require('fs-extra')
+  const request = require('request')
 
-imgDownloader.init(filePath, downloadFolder)
+  function deleteImagesDirectory (dir) {
+    return new Promise((resolve, reject) => {
+      fs.emptyDir(dir, (err) => {
+        if (err)
+          reject(err)
+        else
+          resolve(`${dir} is now empty`)
+      })
+    })
+  }
+
+  function getImages (origin) {
+    const lineReader = readline.createInterface({
+      input: fs.createReadStream(origin)
+    })
+
+    return new Promise((resolve, reject) => {
+      let images = []
+      lineReader.on('line', (line) => images.push(line))
+      lineReader.on('error', () => reject(new Error('Error while reading the file')))
+      lineReader.on('close', () => resolve(images))
+    })
+  }
+
+  function download (images, dest) {
+    Promise.each(images, image => new Promise((resolve, reject) => {
+      let filename = extractFileNameFromUrl(image)
+      console.log('Downloading Image: ' + filename)
+      request(image).on('error', reject).pipe(fs.createWriteStream(path.join(dest, filename))).on('finish', () => {
+          console.log('Downloaded Image: ' + filename)
+          resolve()
+      })
+    })).then(() => {
+        console.log('All images Downloaded!')
+    }).catch(err => {
+        console.error('Failed: ' + err.message)
+    })
+  }
+
+  function extractFileNameFromUrl (imgUrl) {
+    let parsedUrl = url.parse(imgUrl)
+    return path.basename(parsedUrl.pathname)
+  }
+
+  function init (origin, dest) {
+    // First we delete the image directory
+    deleteImagesDirectory(dest)
+    .then((result) => {
+      console.log(result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    // Then we get the images
+    .then(() => {
+      return getImages(origin)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    // Then we proceed to download them one by one
+    .then((images) => {
+      download(images, dest)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  return {
+    init: init
+  }
+})()
